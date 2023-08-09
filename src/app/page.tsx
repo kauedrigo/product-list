7bb21732ce29dd956136bb2afac0473ca7744832
Kaue <kaue.drigo@cybergenios.com.br>
2023-08-09 15:08:09 -0300

    feat: add filter by product name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,37 @@
 
 import { FilterTag } from '@/components/filter-tag'
 import { ProductPreview } from '@/components/product-preview'
-import { ProductList } from '@/types'
+import { Product, ProductList } from '@/types'
 import { getProductListCategoriesWithCount } from '@/utils'
 import { ChangeEvent, useMemo, useState } from 'react'
 import products from '../assets/products.json'
+import { SearchBar } from '@/components/search-bar'
 
 export default function Home() {
 	const productList = products as ProductList
 	const categories = getProductListCategoriesWithCount(productList)
 
 	const [tagFilter, setTagFilter] = useState<string[]>([])
+	const [searchFilter, setSearchFilter] = useState('')
 
 	const filteredProducts = useMemo(() => {
 		const products = productList.data.nodes
-		return tagFilter.length
-			? products.filter((product) => tagFilter.includes(product.category.name))
-			: products
-	}, [tagFilter, productList])
+
+		const filterByName = (products: Product[]) => {
+			return products.filter((product) => product.name.includes(searchFilter))
+		}
+
+		const filterByTag = (products: Product[]) => {
+			return tagFilter.length
+				? products.filter((product) => tagFilter.includes(product.category.name))
+				: products
+		}
+
+		const filteredByName = filterByName(products)
+		const filteredByTag = filterByTag(filteredByName)
+
+		return filteredByTag
+	}, [tagFilter, productList, searchFilter])
 
 	const handleSelectTag = (event: ChangeEvent<HTMLInputElement>) => {
 		const value = event.currentTarget.value
@@ -43,34 +57,37 @@ export default function Home() {
 	}
 
 	return (
-		<div className="w-full min-h-screen grid md:grid-cols-[200px_auto] p-4 md:p-6 gap-6">
-			<div className="w-full flex flex-col gap-6">
-				<p className="text-2xl">Filtros</p>
-				<div className="flex flex-col gap-4 grid-col">
-					{Object.keys(categories).map((category) => (
-						<FilterTag
-							key={category}
-							category={category}
-							count={categories[category]}
-							onChange={handleSelectTag}
-						/>
-					))}
+		<div className="p-4 md:p-6 flex flex-col gap-6">
+			<SearchBar initialValue={searchFilter} setSearchValue={setSearchFilter} />
+			<div className="w-full min-h-screen grid md:grid-cols-[200px_auto] gap-6">
+				<div className="w-full flex flex-col gap-6">
+					<p className="text-2xl">Filtros</p>
+					<div className="flex flex-col gap-4 grid-col">
+						{Object.keys(categories).map((category) => (
+							<FilterTag
+								key={category}
+								category={category}
+								count={categories[category]}
+								onChange={handleSelectTag}
+							/>
+						))}
+					</div>
+					{tagFilter.length > 0 && (
+						<button onClick={resetTagFilters} className="w-max">
+							X Limpiar filtros
+						</button>
+					)}
 				</div>
-				{tagFilter.length > 0 && (
-					<button onClick={resetTagFilters} className="w-max">
-						X Limpiar filtros
-					</button>
-				)}
-			</div>
-			<div className="w-full flex flex-col gap-6">
-				<p className="text-2xl">
-					{filteredProducts.length} {filteredProducts.length === 1 ? 'resultado' : 'resultados'}
-				</p>
+				<div className="w-full flex flex-col gap-6">
+					<p className="text-2xl">
+						{filteredProducts.length} {filteredProducts.length === 1 ? 'resultado' : 'resultados'}
+					</p>
 
-				<div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:gap-x-10">
-					{filteredProducts.map((product) => (
-						<ProductPreview key={product.id} product={product} />
-					))}
+					<div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:gap-x-10">
+						{filteredProducts.map((product) => (
+							<ProductPreview key={product.id} product={product} />
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
